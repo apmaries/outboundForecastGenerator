bd5b3290098a09ccdd61aadfe65c64697614d468
Andy Maries <andy.maries@genesys.com>
2024-01-24 22:15:16 +1100

    Initial commit

---
+++ scheduler copy.js
@@ -0,0 +1,21 @@
+// scheduler.js
+
+function runScheduler(
+  selectedBuId,
+  selectedBuTimeZone,
+  weekStart,
+  numContacts,
+  historicalWeeks,
+  shrinkage
+) {
+  // Your scheduling logic goes here
+  // For demonstration, just log the arguments to the console
+  console.log("Selected BU ID:", selectedBuId);
+  console.log("Selected BU TimeZone:", selectedBuTimeZone);
+  console.log("Week Start:", weekStart);
+  console.log("Number of Contacts:", numContacts);
+  console.log("Historical Weeks:", historicalWeeks);
+  console.log("Shrinkage:", shrinkage + "%");
+
+  // Add more logic as needed
+}
