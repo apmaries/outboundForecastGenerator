4cc9eb32b6661f9fbf036f9b8b29e9776830979f
Andy Maries <andy.maries@genesys.com>
2024-01-30 09:21:36 +1100

    updated console logging

--- src/main.js
+++ src/main.js
@@ -27,11 +27,11 @@ async function runHelper(
   await subscribeToNotificationsChannel(businessUnitId);
 
   // Your existing scheduling logic goes here
-  console.log("Selected BU ID:", businessUnitId);
-  console.log("Selected BU TimeZone:", selectedBuTimeZone);
-  console.log("Week Start:", weekStart);
-  console.log("Number of Contacts:", numContacts);
-  console.log("Historical Weeks:", historicalWeeks);
+  console.log("OFG: Selected BU ID:", businessUnitId);
+  console.log("OFG: Selected BU TimeZone:", selectedBuTimeZone);
+  console.log("OFG: Week Start:", weekStart);
+  console.log("OFG: Number of Contacts:", numContacts);
+  console.log("OFG: Historical Weeks:", historicalWeeks);
 
   // Function to get planning groups from BU id
   async function getPlanningGroups() {
@@ -44,7 +44,7 @@ async function runHelper(
   // Function to get outbound campaigns
   async function getCampaigns() {
     const campaignsArray = [];
-    console.log(`Get Campaigns initiated`);
+    console.log(`OFG: Get Campaigns initiated`);
     console.log(businessUnitId);
     return campaignsArray;
   }
@@ -52,7 +52,7 @@ async function runHelper(
   // Function to get queue campaigns
   async function queueCampaignMatcher() {
     const queueCampaignsArray = [];
-    console.log(`Queue Campaigns initiated`);
+    console.log(`OFG: Queue Campaigns initiated`);
     console.log(businessUnitId);
     return queueCampaignsArray;
   }
@@ -60,7 +60,7 @@ async function runHelper(
   // Function to build query body
   async function queryBuilder(queueCampaigns) {
     const queriesArray = [];
-    console.log(`Query Builder initiated`);
+    console.log(`OFG: Query Builder initiated`);
     console.log(businessUnitId);
     console.log(historicalWeeks);
     return queriesArray;
@@ -69,7 +69,7 @@ async function runHelper(
   // Function to execute queries
   async function executeQueries() {
     const queryResults = [];
-    console.log(`Executing queries`);
+    console.log(`OFG: Executing queries`);
     console.log(businessUnitId);
     return queryResults;
   }
