8dbf0a4c2f07cd00d7c1a3d80904b9b77e02719f
Andy Maries <andy.maries@genesys.com>
2024-01-30 21:01:15 +1100

    updates to flow

--- src/session.js
+++ src/session.js
@@ -15,75 +15,74 @@ if (window.location.hash) {
 
   const token = getParameterByName("access_token");
   sessionStorage.setItem("token", token);
+}
 
-  // get user details
-  async function getUser() {
-    let user = await fetchDataWithRetry(`/api/v2/users/me`, "GET");
-    if (user) {
-      var userName = user.name;
-      var userId = user.id;
-      console.log(`OFG: User ${userName} (${userId}) returned`);
-      var userWelcomeDiv = document.getElementById("user-welcome");
+// get user details
+async function getUser() {
+  let user = await fetchDataWithRetry(`/api/v2/users/me`, "GET");
+  if (user) {
+    var userName = user.name;
+    var userId = user.id;
+    console.log(`OFG: User ${userName} (${userId}) returned`);
+    var userWelcomeDiv = document.getElementById("user-welcome");
 
-      // Create a <p> element with the welcome message
-      const welcomeParagraph = document.createElement("p");
-      welcomeParagraph.textContent = `Welcome ${userName}`;
+    // Create a <p> element with the welcome message
+    const welcomeParagraph = document.createElement("p");
+    welcomeParagraph.textContent = `Welcome ${userName}`;
 
-      // Append the <p> element to the parent node
-      userWelcomeDiv.appendChild(welcomeParagraph);
-      userWelcomeDiv.removeAttribute("hidden");
-    } else {
-      console.error(`OFG: Error getting user`);
-    }
+    // Append the <p> element to the parent node
+    userWelcomeDiv.appendChild(welcomeParagraph);
+    userWelcomeDiv.removeAttribute("hidden");
+  } else {
+    console.error(`OFG: Error getting user`);
+  }
+}
+
+async function getOrgLevelStuff() {
+  // make sure user is authorised before returning more sensitive data
+  await getUser();
+
+  //get oauth client details
+  let client = await fetchDataWithRetry(
+    `/api/v2/oauth/clients/${clientId}`,
+    "GET"
+  );
+  if (client) {
+    console.log("OFG: OAuth client details returned");
+    const clientName = client.name;
+    const clientScope = client.scope;
+    sessionStorage.setItem("clientName", clientName);
+    sessionStorage.setItem("clientScope", clientScope);
+  } else {
+    console.error(`OFG: Error getting OAuth client`);
   }
 
-  async function getTheRest() {
-    // make sure user is authorised before returning more sensitive data
-    await getUser();
-
-    //get oauth client details
-    let client = await fetchDataWithRetry(
-      `/api/v2/oauth/clients/${clientId}`,
-      "GET"
-    );
-    if (client) {
-      console.log("OFG: OAuth client details returned");
-      const clientName = client.name;
-      const clientScope = client.scope;
-      sessionStorage.setItem("clientName", clientName);
-      sessionStorage.setItem("clientScope", clientScope);
-    } else {
-      console.error(`OFG: Error getting OAuth client`);
-    }
-
-    //get divisions list
-    let divisions = await fetchDataWithRetry(
-      `/api/v2/authorization/divisions?pageSize=1000&pageNumber=1`,
-      "GET"
-    );
-    if (divisions) {
-      console.log("OFG: Divisions data returned");
-      sessionStorage.setItem("divisionsList", JSON.stringify(divisions));
-    } else {
-      console.error(`OFG: Error getting divisions`);
-    }
-
-    //create notification channel
-    let channel = await fetchDataWithRetry(
-      `/api/v2/notifications/channels`,
-      "POST"
-    );
-    if (channel) {
-      console.log("OFG: Notifications channel opened");
-      const notificationsUri = channel.connectUri;
-      const notificationsId = channel.id;
-      sessionStorage.setItem("notificationsUri", notificationsUri);
-      sessionStorage.setItem("notificationsId", notificationsId);
-    } else {
-      console.error(`OFG: Error creating notifications channel`);
-    }
+  //get divisions list
+  let divisions = await fetchDataWithRetry(
+    `/api/v2/authorization/divisions?pageSize=1000&pageNumber=1`,
+    "GET"
+  );
+  if (divisions) {
+    console.log("OFG: Divisions data returned");
+    sessionStorage.setItem("divisionsList", JSON.stringify(divisions));
+  } else {
+    console.error(`OFG: Error getting divisions`);
   }
-  getTheRest();
+
+  //create notification channel
+  let channel = await fetchDataWithRetry(
+    `/api/v2/notifications/channels`,
+    "POST"
+  );
+  if (channel) {
+    console.log("OFG: Notifications channel opened");
+    const notificationsUri = channel.connectUri;
+    const notificationsId = channel.id;
+    sessionStorage.setItem("notificationsUri", notificationsUri);
+    sessionStorage.setItem("notificationsId", notificationsId);
+  } else {
+    console.error(`OFG: Error creating notifications channel`);
+  }
 }
 
 // auto timeout
