34823f071a7f988addd685cfb0ad1d1848bed839
Andy Maries <andy.maries@genesys.com>
2024-02-08 23:48:19 +1100

    Refactor numberCruncher.js to calculate and log contact rate

--- src/numberCruncher.js
+++ src/numberCruncher.js
@@ -11,27 +11,39 @@ function crunchNumbers(body) {
     const [startString, _] = interval.split("/");
     const startDate = new Date(startString);
     const localDateTimeString = startDate.toLocaleString();
-    const utcDateTimeString = startDate.toISOString();
-
-    // temp log
-    console.log(
-      startString + " = " + localDateTimeString + " = " + utcDateTimeString
-    );
 
     // Day name en-AU locale
     const dayNameAU = startDate.toLocaleDateString("en-AU", {
       weekday: "long",
     });
 
-    // temp log
-    console.log(dayNameAU);
+    // Get interval index number
+    const totalMinutes = hours * 60 + minutes;
+    const intervalDuration = 15;
+    const indexNumber = Math.floor(totalMinutes / intervalDuration);
+
+    // Prep data
+
+    // Extract interval level metrics
+    const attemptedMetrics = metrics.find(
+      (metric) => metric.metric === "nOutboundAttempted"
+    );
+    const connectedMetrics = metrics.find(
+      (metric) => metric.metric === "nOutboundConnected"
+    );
+
+    // Error checking: Ensure both metrics exist and count is not zero
+    if (
+      attemptedMetrics &&
+      connectedMetrics &&
+      attemptedMetrics.stats.count !== 0
+    ) {
+      const contactRate =
+        connectedMetrics.stats.count / attemptedMetrics.stats.count;
+      console.log(`OFG: ${localDateTimeString} Contact Rate: ${contactRate}`);
+    } else {
+      console.log("Error: Metrics not available or divide by zero error.");
+      console.log(metrics);
+    }
   }
-}
-
-function weeklyNumbersCruncher(data, timeZone) {
-  console.log(`OFG: Weekly numbers being crunched`);
-}
-
-function dailyNumbersCruncher(data, timeZone) {
-  console.log(`OFG: Daily numbers being crunched`);
 }
