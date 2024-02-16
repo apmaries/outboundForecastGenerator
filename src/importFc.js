3c519d5b3c3bdb4034cbf0468babe0a2d3936add
Andy Maries <andy.maries@genesys.com>
2024-02-16 14:42:53 +1100

    Add importUrl functionality to importFc function

--- src/importFc.js
+++ src/importFc.js
@@ -7,5 +7,16 @@ async function prepFcImportBody(campaignsData) {
 }
 
 function importFc(fcImportBody) {
+  const businessUnitId = fcImportBody.buId;
+  const weekDateId = fcImportBody.weekDateId;
+
   console.log("OFG: importFc with body", fcImportBody);
+
+  const importUrl = fetchDataWithRetry(
+    `/api/v2/workforcemanagement/businessunits/${businessUnitId}/weeks/${weekDateId}/shorttermforecasts/import/uploadurl`,
+    "POST",
+    {
+      "contentLengthBytes": 1123,
+    }
+  );
 }
