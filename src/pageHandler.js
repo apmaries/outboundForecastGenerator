7903816c9f7b1beade59525a4f7d2dbd60d68508
Andy Maries <andy.maries@genesys.com>
2024-03-27 13:50:43 +1100

    Add business unit loading functionality and dropdown population

--- src/pageHandler.js
+++ src/pageHandler.js
@@ -1,3 +1,10 @@
+import { handleApiCalls, globalPageOpts } from "./apiHandler.js";
+import { populateDropdown } from "./dropdownHandler.js";
+
+// Global variables
+let businessUnits = [];
+const businessUnitListbox = document.getElementById("business-unit-listbox");
+
 // Function to hide loading spinner and show content
 export async function hideLoadingSpinner(spinner, elem) {
   console.log("OFG: hideLoadingSpinner");
@@ -21,4 +28,38 @@ export function switchPages(currentPageId, nextPageId) {
     nextPage.classList.add("active-page");
     nextPage.classList.remove("inactive-page");
   }, 100); // Delay for a smoother transition
+}
+
+export async function loadPageOne() {
+  if (window.isTesting) {
+    // Testing mode - Get Business Units from ./test/source/businessUnits.json
+    try {
+      const response = await fetch(
+        "/outboundForecastGenerator/test/source/businessUnits.json"
+      );
+      const data = await response.json();
+      businessUnits = data.entities;
+      console.log(
+        `[OFG] ${businessUnits.length} Business Units loaded from test data`,
+        businessUnits
+      );
+    } catch (error) {
+      console.error("[OFG] Error loading test data", error);
+    }
+  } else {
+    // Production mode
+    businessUnits = await handleApiCalls(
+      "WorkforceManagementApi.getWorkforcemanagementBusinessunits"
+    );
+    console.log(
+      `[OFG] ${businessUnits.length} Business Units loaded`,
+      businessUnits
+    );
+  }
+
+  // Populate Business Unit dropdown
+  await populateDropdown(businessUnitListbox, businessUnits);
+
+  // Hide loading spinner and show main
+  await hideLoadingSpinner("main-loading-section", "main");
 }
