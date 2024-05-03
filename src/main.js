d70a4c5419dbeaf79384b83ae963e5204807226b
Andy Maries <andy.maries@genesys.com>
2024-05-03 12:38:24 +1000

    chore: Load test data and replace planning group IDs in main.js

--- src/main.js
+++ src/main.js
@@ -94,7 +94,32 @@ export async function runGenerator(
   async function executeQueries() {
     let queryResults = [];
     console.log(`[OFG] Executing queries`);
-    console.log(businessUnitId);
+
+    // Needs to be completed - using test data for now
+    // load test data
+    fetch("./test/testData.json")
+      .then((response) => response.json())
+      .then((testData) => {
+        // replace planning group id's in test data with actual planning group id's from sco org
+        for (let i = 0; i < testData.length; i++) {
+          let campaign1 = "c1a07179-b2f2-4251-a1fa-9fd9b3219174";
+          let campaign2 = "ce713659-c13a-486e-b978-28b77436bf67";
+          let amCampaign1 = "dc853e3b-0c45-42c1-9e34-b52567f5a3c7";
+          let amCampaign2 = "1896f07f-7a5c-4132-9f31-2ad69dd4435f";
+
+          if (testData[i].group.outboundCampaignId === campaign1) {
+            testData[i].group.outboundCampaignId = amCampaign1;
+          } else if (testData[i].group.outboundCampaignId === campaign2) {
+            testData[i].group.outboundCampaignId = amCampaign2;
+          }
+        }
+        queryResults = testData;
+        console.log("[OFG] Test data loaded");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
     return queryResults;
   }
 
