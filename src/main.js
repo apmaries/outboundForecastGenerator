94ed751f85e69a1d29e41c4b08bb57e22ad5d716
Andy Maries <andy.maries@genesys.com>
2024-05-13 10:12:30 +1000

    refactor: Remove queryBuilder and executeQueries functions from main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import {
   switchPages,
   loadPageOne,
 } from "./pageHandler.js";
-import { handleApiCalls, subscribeToNotifications } from "./apiHandler.js";
+import { subscribeToNotifications } from "./apiHandler.js";
+import { queryBuilder, executeQueries } from "./queryHandler.js";
 import {
   prepFcMetrics,
   groupByIndexNumber,
@@ -70,48 +71,6 @@ export async function runGenerator(
   function updateLoadingMessage(message) {
     const loadingMessage = document.getElementById("loading-message");
     loadingMessage.innerHTML = message;
-  }
-
-  // Function to build query body
-  async function queryBuilder() {
-    let queriesArray = [];
-    console.log(`[OFG] Query Builder initiated`);
-    console.debug(
-      "[OFG] planningGroupContactsArray: ",
-      planningGroupContactsArray
-    );
-    console.debug("[OFG] historicalWeeks: ", historicalWeeks);
-    return queriesArray;
-  }
-
-  // Function to execute queries
-  async function executeQueries() {
-    console.log(`[OFG] Executing queries`);
-
-    // Needs to be completed - using test data for now
-    // load test data
-    return fetch("./test/testData.json")
-      .then((response) => response.json())
-      .then((testData) => {
-        // replace planning group id's in test data with actual planning group id's from sco org
-        for (let i = 0; i < testData.length; i++) {
-          let campaign1 = "c1a07179-b2f2-4251-a1fa-9fd9b3219174";
-          let campaign2 = "ce713659-c13a-486e-b978-28b77436bf67";
-          let amCampaign1 = "dc853e3b-0c45-42c1-9e34-b52567f5a3c7";
-          let amCampaign2 = "1896f07f-7a5c-4132-9f31-2ad69dd4435f";
-
-          if (testData[i].group.outboundCampaignId === campaign1) {
-            testData[i].group.outboundCampaignId = amCampaign1;
-          } else if (testData[i].group.outboundCampaignId === campaign2) {
-            testData[i].group.outboundCampaignId = amCampaign2;
-          }
-        }
-        console.log("[OFG] Test data loaded");
-        return testData;
-      })
-      .catch((error) => {
-        console.error(error);
-      });
   }
 
   // Returns the ISO week of the date.
