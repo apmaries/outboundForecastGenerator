11f68c0d382ea8a11ae87bb2fe681d603d7c3a30
Andy Maries <andy.maries@genesys.com>
2024-05-21 09:44:38 +1000

    refactor: Update sessionHandler.js to improve user authorization and notifications handling

--- src/sessionHandler.js
+++ src/sessionHandler.js
@@ -1,115 +1,92 @@
 import { handleApiCalls, toastUser } from "./apiHandler.js";
 
+// Declare global variables
+const indexPage = "./not-authorized.html";
+const isTesting = window.isTesting;
+
+// Function to check if user is authorised
+function internalUserCheck(emailAddress) {
+  const domain = emailAddress.split("@")[1];
+  if (domain.toLowerCase() === "genesys.com") {
+    console.log("[OFG] Authorised user");
+  } else {
+    console.log("[OFG] Unauthorised user!");
+    alert("Sorry, you are not authorised to use this page :(");
+    window.location.replace(indexPage);
+  }
+}
+
+// Function to get user details and check if authorised
+async function getUser() {
+  console.log("[OFG] Getting user details");
+  // Get user welcome div in page
+  let userWelcome = document.getElementById("user-welcome");
+
+  if (isTesting) {
+    console.log("[OFG] Testing mode enabled. Skipping user details");
+    userWelcome.innerHTML = `Welcome, Test User!`;
+  } else {
+    try {
+      let udata = await handleApiCalls("UsersApi.getUsersMe");
+
+      if (udata) {
+        console.log("[OFG] User details returned", udata);
+        const userName = udata.name;
+        const userId = udata.id;
+        const userEmail = udata.email;
+
+        internalUserCheck(userEmail);
+
+        sessionStorage.setItem("user_name", userName);
+        sessionStorage.setItem("user_id", userId);
+
+        // Toast not required if can't include custom buttons in toast to open generated forecast
+        //toastUser();
+
+        userWelcome.innerHTML = `Welcome, ${userName}!`;
+      } else {
+        console.error(`[OFG] Error getting user details. `, udata);
+        window.location.replace(indexPage);
+        throw new Error("Error getting user details");
+      }
+    } catch (error) {
+      console.error(`[OFG] Error getting user`, error);
+      window.location.replace(indexPage);
+      throw error;
+    }
+  }
+  userWelcome.removeAttribute("hidden");
+}
+
+// Function to open notification channel
+async function openNotificationsChannel() {
+  if (!isTesting) {
+    console.log("[OFG] Opening notifications channel");
+    let channel = await handleApiCalls(
+      "NotificationsApi.postNotificationsChannels"
+    );
+
+    if (channel) {
+      console.log("[OFG] Notifications channel opened");
+      const notificationsUri = channel.connectUri;
+      const notificationsId = channel.id;
+      sessionStorage.setItem("notifications_uri", notificationsUri);
+      sessionStorage.setItem("notifications_id", notificationsId);
+    } else {
+      console.error(`[OFG] Error creating notifications channel`);
+    }
+  }
+}
+
 export async function startSession() {
   console.log("[OFG] Starting session");
 
-  // Declare global variables
-  const indexPage = "./not-authorized.html";
-  const isTesting = window.isTesting;
-
-  // Functions start here
-  /* don't need this anymore
-  function getParameterByName(name) {
-    name = name.replace(/[\\[]/, "\\[").replace(/[\]]/, "\\]");
-    var regex = new RegExp("[\\#&]" + name + "=([^&#]*)"),
-      results = regex.exec(location.hash);
-    return results === null
-      ? ""
-      : decodeURIComponent(results[1].replace(/\+/g, " "));
-  }
-  */
-
-  // check if account being used to log in with is internal genesys
-  function internalUserCheck(emailAddress) {
-    const domain = emailAddress.split("@")[1];
-    if (domain.toLowerCase() === "genesys.com") {
-      console.log("[OFG] Authorised user");
-    } else {
-      console.log("[OFG] Unauthorised user!");
-      alert("Sorry, you are not authorised to use this page :(");
-      window.location.replace(indexPage);
-    }
-  }
-
-  // get user details
-  async function getUser() {
-    console.log("[OFG] Getting user details");
-    // Get user welcome div in page
-    let userWelcome = document.getElementById("user-welcome");
-
-    if (isTesting) {
-      console.log("[OFG] Testing mode enabled. Skipping user details");
-      userWelcome.innerHTML = `Welcome, Test User!`;
-    } else {
-      try {
-        let udata = await handleApiCalls("UsersApi.getUsersMe");
-
-        if (udata) {
-          console.log("[OFG] User details returned", udata);
-          const userName = udata.name;
-          const userId = udata.id;
-          const userEmail = udata.email;
-
-          internalUserCheck(userEmail);
-
-          sessionStorage.setItem("user_name", userName);
-          sessionStorage.setItem("user_id", userId);
-
-          // Toast not required if can't include custom buttons in toast to open generated forecast
-          //toastUser();
-
-          userWelcome.innerHTML = `Welcome, ${userName}!`;
-        } else {
-          console.error(`[OFG] Error getting user details. `, udata);
-          window.location.replace(indexPage);
-        }
-      } catch (error) {
-        console.error(`[OFG] Error getting user`, error);
-        window.location.replace(indexPage);
-      }
-    }
-    userWelcome.removeAttribute("hidden");
-  }
-
-  // open notification channel
-  async function openNotificationsChannel() {
-    if (!isTesting) {
-      console.log("[OFG] Opening notifications channel");
-      let channel = await handleApiCalls(
-        "NotificationsApi.postNotificationsChannels"
-      );
-
-      if (channel) {
-        console.log("[OFG] Notifications channel opened");
-        const notificationsUri = channel.connectUri;
-        const notificationsId = channel.id;
-        sessionStorage.setItem("notifications_uri", notificationsUri);
-        sessionStorage.setItem("notifications_id", notificationsId);
-      } else {
-        console.error(`[OFG] Error creating notifications channel`);
-      }
-    }
-  }
-
-  // Functions end here
-
-  // main code starts here
-
-  /*
-  if (window.location.hash) {
-    // Set the token in session storage
-    console.log("[OFG] Retrieving access token");
-    const token = getParameterByName("access_token");
-    sessionStorage.setItem("oauth_token", token);
-
-    // make sure user is authorised before continuing
+  try {
+    // Get user details and open notifications channel
     await getUser();
     await openNotificationsChannel();
+    console.log("[OFG] Session started");
+  } catch (error) {
+    console.error(`[OFG] Error starting session: `, error);
   }
-  */
-
-  // make sure user is authorised before continuing
-  await getUser();
-  await openNotificationsChannel();
-  console.log("[OFG] Session started");
 }
