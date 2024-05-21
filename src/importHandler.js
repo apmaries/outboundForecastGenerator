5bb360a579c7eacbaaf0b7e581fee02749386be1
Andy Maries <andy.maries@genesys.com>
2024-05-21 14:03:51 +1000

    refactor: Update importHandler.js to optimize forecast data processing

--- src/importHandler.js
+++ src/importHandler.js
@@ -2,27 +2,37 @@ import { handleApiCalls } from "./apiHandler.js";
 import { downloadJson, downloadGzip } from "./pageHandler.js";
 
 export async function prepFcImportBody(
-  campaignsData,
+  forecastData,
   buStartDayOfWeek,
   description
 ) {
   console.log("[OFG] Preparing Forecast Import Body and encoding to gzip");
+  console.log("[OFG] Preparing forecast data: ", forecastData);
 
-  // function to gzip encode the body
+  // Function to gzip encode the body
   function gzipEncode(body) {
     const encoder = new TextEncoder();
     const data = encoder.encode(JSON.stringify(body));
     return pako.gzip(data);
   }
 
-  // build the body for the forecast import
+  // Function to round the values to 2 decimal places
+  function roundToTwo(num) {
+    return +(Math.round(num + "e+2") + "e-2");
+  }
+
+  // Build the body for the forecast import
   let planningGroupsArray = [];
 
-  for (let i = 0; i < campaignsData.length; i++) {
-    const campaign = campaignsData[i];
+  for (let i = 0; i < forecastData.length; i++) {
+    const campaign = forecastData[i];
     const campaigPgId = campaign.pgId;
+    console.log(`[OFG] Processing forecast for Planning Group: ${campaigPgId}`);
 
     // Reorder arrays to align to BU start day of week
+    console.debug(
+      `[OFG] ${campaigPgId}: Reordering forecast data to ${buStartDayOfWeek} week start`
+    );
     const contactsIntraday = campaign.fcData.contactsIntraday;
     const ahtIntraday = campaign.fcData.ahtIntraday;
 
@@ -47,26 +57,37 @@ export async function prepFcImportBody(
       ahtIntradayReordered.push(ahtIntraday[index]);
     }
 
+    // Replicate the new 0 index at the end of the arrays
+    console.debug(
+      `[OFG] ${campaigPgId}: Adding required 8th day to reordered forecast data`
+    );
+    contactsIntradayReordered.push(contactsIntradayReordered[0]);
+    ahtIntradayReordered.push(ahtIntradayReordered[0]);
+
+    // Flatten the arrays
+    console.debug(`[OFG] ${campaigPgId}: Flattening reordered forecast data`);
     const offeredPerInterval = contactsIntradayReordered.flat();
     const averageHandleTimeSecondsPerInterval = ahtIntradayReordered.flat();
 
-    // function to round the values to 2 decimal places
-    function roundToTwo(num) {
-      return +(Math.round(num + "e+2") + "e-2");
+    // Round data per interval to 2 decimal places
+    console.debug(
+      `[OFG] ${campaigPgId}: Rounding forecast data to 2 decimal places`
+    );
+    // offered
+    for (let i = 0; i < offeredPerInterval.length; i++) {
+      offeredPerInterval[i] = roundToTwo(offeredPerInterval[i]);
     }
-
-    // round the average handle time to 2 decimal places
+    // aht
     for (let i = 0; i < averageHandleTimeSecondsPerInterval.length; i++) {
       averageHandleTimeSecondsPerInterval[i] = roundToTwo(
         averageHandleTimeSecondsPerInterval[i]
       );
     }
 
-    // round the offered per interval to 2 decimal places
-    for (let i = 0; i < offeredPerInterval.length; i++) {
-      offeredPerInterval[i] = roundToTwo(offeredPerInterval[i]);
-    }
-
+    // Create the object for the planning group
+    console.debug(
+      `[OFG] ${campaigPgId}: Creating Planning Group object for import body`
+    );
     let pgObj = {
       "planningGroupId": campaigPgId,
       "offeredPerInterval": offeredPerInterval,
@@ -76,6 +97,8 @@ export async function prepFcImportBody(
     planningGroupsArray.push(pgObj);
   }
 
+  // Create the forecast import body
+  console.debug("[OFG] Creating Forecast Import Body");
   let fcImportBody = {
     "description": description,
     "weekCount": 1,
@@ -112,73 +135,7 @@ export async function generateUrl(
   return importUrl;
 }
 
-export async function importFcOld(
-  businessUnitId,
-  weekDateId,
-  gzip,
-  uploadAttributes
-) {
-  console.log("[OFG] Uploading forecast");
-
-  const uploadKey = uploadAttributes.uploadKey;
-  const uploadUrl = uploadAttributes.url;
-  const uploadHeaders = uploadAttributes.headers;
-
-  //temp logging
-  console.debug("[OFG] Upload Key: ", uploadKey);
-  console.debug("[OFG] Upload URL: ", uploadUrl);
-  console.debug("[OFG] Upload Headers: " + JSON.stringify(uploadHeaders));
-  console.debug("[OFG] gzip: ", gzip);
-  console.debug(JSON.stringify(uploadHeaders));
-  console.debug(uploadHeaders);
-  // this all looks fine but the upload is being blocked on CORS :(
-
-  // download the gzip file
-  downloadGzip(gzip);
-
-  /*
-  // upload gzip to upload url with uploadHeaders
-  const uploadResponse = await fetch(uploadUrl, {
-    method: "PUT",
-    headers: uploadHeaders,
-    body: gzip,
-  });
-
-  // Add debugging for response
-  console.log(
-    "[OFG] Upload Response: ",
-    uploadResponse.status,
-    uploadResponse.statusText
-  );
-
-  // check if upload was successful
-  if (uploadResponse.ok) {
-    console.log("[OFG] Forecast uploaded successfully");
-
-    // complete the import
-    const importAttributes = {
-      "uploadKey": uploadKey,
-    };
-
-    const importResponse = await handleApiCalls(
-      "WorkforceManagementApi.postWorkforcemanagementBusinessunitWeekShorttermforecastsImport",
-      businessUnitId, // Pass selected Business Unit ID
-      weekDateId, // Pass selected Week Date ID
-      {
-        "uploadKey": uploadKey,
-      }
-    );
-
-    console.log("[OFG] Forecast import complete");
-    return importResponse;
-  } else {
-    console.error("[OFG] Forecast upload failed");
-    return null;
-  }
-  */
-}
-
-export async function invokeGCF(uploadAttributes, campaignsData) {
+export async function invokeGCF(uploadAttributes, forecastData) {
   console.log("[OFG] Invoking GCF");
   // Get client id from session storage
   const clientId = sessionStorage.getItem("oauth_client");
@@ -194,7 +151,7 @@ export async function invokeGCF(uploadAttributes, campaignsData) {
   const data = {
     url: uploadUrl,
     header: uploadHeaders,
-    data: campaignsData,
+    data: forecastData,
   };
 
   const response = await fetch(url, {
