23e7614c8d5b57d6a060b5f813e7ff8d1f88057e
Andy Maries <andy.maries@genesys.com>
2024-05-28 16:42:51 +1000

    refactor: Add steps comments

--- src/inboundHandler.js
+++ src/inboundHandler.js
@@ -4,5 +4,10 @@ import { handleApiCalls, globalPageOpts } from "./apiHandler.js";
 let generateOperationId = null;
 
 export function generateInboundForecast(method) {
+  // Subscribe to generate notifications
   // Check inbound forecast generation method
+  // Generate the forecast
+  // Get the forecast data
+  // Add inbound forecast data to mainPgForecastData
+  // Return the forecast data
 }
